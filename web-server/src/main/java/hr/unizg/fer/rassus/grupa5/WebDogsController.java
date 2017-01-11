674afeca4963b9dd28a8dbe76cdc94aad77f74c3
ab47813 <ana.bozic@fer.hr>
2017-01-11 19:03:48 +0100

    dovrsen web server za pse

--- web-server/src/main/java/hr/unizg/fer/rassus/grupa5/WebDogsController.java
+++ web-server/src/main/java/hr/unizg/fer/rassus/grupa5/WebDogsController.java
@@ -4,10 +4,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping("/dogs/*")
@@ -19,12 +21,43 @@ public class WebDogsController {
 	public WebDogsController(WebDogsService dogsService) {
 		this.dogsService = dogsService;
 	}
+	
+	@RequestMapping(value = "/allDogs", method = RequestMethod.GET)
+	public String getall(Model model) {
+		List<Dog> registrations = new ArrayList<Dog>();
+		registrations = dogsService.getall();
+		model.addAttribute("dogList", registrations);
+		return "dogs-all";
+	}
+	
+	@RequestMapping(value = "/register", method = RequestMethod.GET)
+	public String register(Model model) {
+		model.addAttribute("dogsList", new Dog());
+		return "dog-list";
+	}
+	
+	@RequestMapping(value = "/register", method = RequestMethod.POST,
+			consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE,
+			produces=MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+	public String createRegistration(Dog registration) {
+		return dogsService.register(registration);
+	}
+	
+	@RequestMapping("/{DogNameById}")
+	public String byDogNamebyId(Model model, @PathVariable("dogId") Long Id){
+		Dog dog = new Dog();
+		dog = (Dog) dogsService.findById(Id);
+		String dogName = dog.getName();
+		model.addAttribute("dogList", dogName);
+		return "dog-list";
+		
+	}
 
 	@RequestMapping("/{dogId}")
 	public String byDogId(Model model, @PathVariable("dogId") Long Id){
 		List<Dog> dogs = new ArrayList<Dog>();
 		dogs = dogsService.findById(Id);
-		model.addAttribute("dogs", dogs);
+		model.addAttribute("dogList", dogs);
 		return "dog-list";
 		
 	}
@@ -33,7 +66,7 @@ public class WebDogsController {
 	public String byOwner(Model model,@PathVariable("dogOwner") String dogOwner) {
 		List<Dog> dogs = new ArrayList<Dog>();
 		dogs = dogsService.findByOwner(dogOwner);
-		model.addAttribute("dogs", dogs);
+		model.addAttribute("dogList", dogs);
 		return "dog-list";
 	}
 	
--- web-server/src/main/java/hr/unizg/fer/rassus/grupa5/WebDogsService.java
+++ web-server/src/main/java/hr/unizg/fer/rassus/grupa5/WebDogsService.java
@@ -1,5 +1,6 @@
 package hr.unizg.fer.rassus.grupa5;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -19,6 +20,37 @@ public class WebDogsService {
 
 	public WebDogsService(String dogsServiceUrl) {
 		this.dogsServiceUrl = dogsServiceUrl.startsWith("http") ? dogsServiceUrl : "http://" + dogsServiceUrl;
+	}
+	
+	List<Dog> getall() {
+
+		Dog[] regs = null;
+		List<Dog> listDogRegistrations = new ArrayList<Dog>();
+		regs = restTemplate.getForObject(dogsServiceUrl + "/allDogs", Dog[].class);
+		System.out.println("all dogs: "+ regs);
+		if (regs == null || regs.length == 0){
+			System.out.println("no dogs registered");
+			Dog emptyreg = new Dog();
+			listDogRegistrations.add(emptyreg);
+		}
+		else{
+			listDogRegistrations.addAll(Arrays.asList(regs));
+			//listDogRegistrations.add(regs);
+		}
+		return listDogRegistrations;
+	}
+	
+	
+	public String register(Dog registration) {
+		restTemplate.postForObject(dogsServiceUrl + "/create", registration, Dog.class);
+		return "dog-list";
+	}
+	
+	String findDogNameById(Long dogId){
+		Dog dog = null;
+		dog = restTemplate.getForObject(dogsServiceUrl + "/{dogId}", Dog.class, dogId);
+		String dogName = dog.getName();
+		return dogName;
 	}
 
 	List<Dog> findById(Long dogId) {
